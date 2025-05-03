22fe9480cd31d4051c977b63f123ba772e210f8c
Arda Tüzün <ardatuzun@Ardas-MacBook-Pro.local>
2025-05-03 14:31:20 +0300

    Bug fixed for bomb creation

--- src/main/java/fr/ubx/poo/ubgarden/game/go/personage/Wasp.java
+++ src/main/java/fr/ubx/poo/ubgarden/game/go/personage/Wasp.java
@@ -6,9 +6,15 @@ import fr.ubx.poo.ubgarden.game.Position;
 import fr.ubx.poo.ubgarden.game.engine.Timer;
 import fr.ubx.poo.ubgarden.game.go.GameObject;
 import fr.ubx.poo.ubgarden.game.go.Movable;
+import fr.ubx.poo.ubgarden.game.go.bonus.InsecticideBomb;
 import fr.ubx.poo.ubgarden.game.go.decor.Decor;
 import fr.ubx.poo.ubgarden.game.go.decor.Door;
 import fr.ubx.poo.ubgarden.game.go.decor.Tree;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Random;
 
 import static fr.ubx.poo.ubgarden.game.Direction.*;
 
@@ -16,5 +22,32 @@ public class Wasp extends Bugs implements Movable {
     private static final int DAMAGE = 20;
     public Wasp(Game game, Position position) {
         super(game, position,game.configuration().waspMoveFrequency()*1000,1,DAMAGE);
+    }
+
+    @Override
+    public void createBomb() {
+        java.util.Map<Position, Decor> decorsWithoutBonus = new HashMap<>();
+        for (Decor decor : game.world().getGrid().values()) {
+            if (decor.getBonus() == null && decor.walkableBy(game.getGardener())) {
+                decorsWithoutBonus.put(decor.getPosition(), decor);
+            }
+        }
+        if (!decorsWithoutBonus.isEmpty()) {
+            List<Decor> eligibleDecors = new ArrayList<>(decorsWithoutBonus.values());
+            Random rand = new Random();
+            Decor randomDecor1 = eligibleDecors.get(rand.nextInt(eligibleDecors.size()));
+            Decor randomDecor2;
+            do {
+                randomDecor2 = eligibleDecors.get(rand.nextInt(eligibleDecors.size()));
+            } while (randomDecor1 == randomDecor2);
+            Position pos1 = randomDecor1.getPosition();
+            Position pos2 = randomDecor2.getPosition();
+            Decor targetDecor1 = game.world().getGrid().get(pos1);
+            Decor targetDecor2 = game.world().getGrid().get(pos2);
+            targetDecor1.setBonus(new InsecticideBomb(pos1, targetDecor1));
+            targetDecor1.getBonus().setModified(true);
+            targetDecor2.setBonus(new InsecticideBomb(pos2, targetDecor2));
+            targetDecor2.getBonus().setModified(true);
+        }
     }
 }
